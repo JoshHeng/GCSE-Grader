786a06a9441f9471ca9e8fac009fb2cc37d8fffe
Josh Heng <josh@joshheng.co.uk>
2020-06-21 20:37:18 +0100

    Implement subject predicted grades

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const slotNum = 15;
+var gradeTotal = 0;
 var grades = [
 	{ grade: 'U', proportion: 1.7 },
 	{ grade: '1', proportion: 4.5 },
@@ -11,63 +11,162 @@ var grades = [
 	{ grade: '8', proportion: 6.7 },
 	{ grade: '9', proportion: 4.5 }
 ];
-var gradeTotal = 0;
+const subjects = [
+	{ id: 'maths', name: 'Maths' },
+	{ id: 'ad-maths', name: 'Additional Maths' },
+	{ id: 'stats', name: 'Statistics' },
+	{ id: 'eng-lang', name: 'English Language' },
+	{ id: 'eng-lit', name: 'English Literature' },
+	{ id: 'science', name: 'Science' },
+	{ id: 'biology', name: 'Biology' },
+	{ id: 'chemistry', name: 'Chemistry' },
+	{ id: 'health', name: 'Health & Social Care' },
+	{ id: 'physics', name: 'Physics' },
+	{ id: 'spanish', name: 'Spanish' },
+	{ id: 'french', name: 'French' },
+	{ id: 'german', name: 'German' },
+	{ id: 'computing', name: 'Computing' },
+	{ id: 'dt', name: 'Design & Technology' },
+	{ id: 'art', name: 'Art & Design' },
+	{ id: 'food', name: 'Food Preparation & Nutrition' },
+	{ id: 'history', name: 'History' },
+	{ id: 'geography', name: 'Geography' },
+	{ id: 'rs', name: 'Religious Studies' },
+	{ id: 'humanities', name: 'Humanities' },
+	{ id: 'business', name: 'Business Studies' },
+	{ id: 'economics', name: 'Economics' },
+	{ id: 'drama', name: 'Performing / Expressive Arts (Drama)' },
+	{ id: 'media', name: 'Media / Film / TV Studies' },
+	{ id: 'music', name: 'Music' }
+]
+var canSpin = false;
 
-function getGrade() {
-	let randomNumber = Math.floor(Math.random()*gradeTotal);
+// Add Predicted Grades
+var predictedGrades = {};
+for (let subject of subjects) {
+	$('#predicted-grades').append(`
+	<div class="subject">
+		<h3>${subject.name}</h3>
+		<form onsubmit="return false">
+			<select name="grade" id="${subject.id}">
+				<option></option>
+				<option>U</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+			</select>
+		</form>
+	</div>`);
 
-	for (grade of grades) {
-		if (randomNumber <= grade.total) return grade.grade;
-	}
-}
+	$('#'+subject.id).on('change', (event) => {
+		if (event.target.value) {
+			for (let grade of grades) {
+				gradeTotal += grade.proportion
+				grade.total = gradeTotal;
+			}
+			predictedGrades[subject.name] = { total: 0, grades: [], grade: event.target.value };
 
-for (grade of grades) {
-	gradeTotal += grade.proportion
-	grade.total = gradeTotal;
-}
+			let gradeIndex = grades.findIndex(grade => grade.grade == event.target.value);
+			if (gradeIndex === -1) return;
 
-for (let i = 0; i < slotNum; i++) {
-	let slot = $('<div class="slot"></div></div>');
-	let slotGrades = $('<div class="slot-grades"></div>');
+			for (let i = 0; i < grades.length && i < gradeIndex + 1; i++) {
+				let total = grades[i].proportion;
 
-	for (let z = 0; z < 3; z++) {
-		let grade = getGrade();
-		slotGrades.append(`<div class="slot-grade">${grade}</div>`);
-	}
-
-	slot.append(slotGrades);
-	$('#slots').append(slot)
-}
-$('#slots').append('<div class="slot-target"></div>');
+				if (i === gradeIndex) total = total * 4;
+				else if (i+1 === gradeIndex) total = total * 4;
+				else if (i+2 === gradeIndex) total = total * 3;
+				else if (i+3 === gradeIndex) total = total * 2;
+				else if (i+4 === gradeIndex) total = total * 1;
+				else total = total * 0.5;
 
 
-let spinning = false;
-function spin() {
-	if (spinning) return;
-	spinning = true;
-
-	$('#slots').empty();
-	$('#spin-button').text('Generating Grades...');
-	$('#spin-button').css('cursor', 'not-allowed');
-
-	let longestDuration = 0;
-	for (let i = 0; i < slotNum; i++) {
-		let slot = $('<div class="slot"></div></div>');
-		let duration = Math.random()*4+4;
-		if (duration > longestDuration) longestDuration = duration;
-
-		let slotGrades = $(`<div class="slot-grades" style="animation-name: spin; animation-duration: ${duration}s"></div>`);
-
-		for (let z = 0; z < 41; z++) {
-			let grade = getGrade();
-			if (z === 39) slotGrades.append(`<div class="slot-grade slot-grade-actual">${grade}</div>`);
-			else slotGrades.append(`<div class="slot-grade slot-grade-fill">${grade}</div>`);
+				predictedGrades[subject.name].total += total;
+				predictedGrades[subject.name].grades.push({
+					grade: grades[i].grade,
+					total: predictedGrades[subject.name].total
+				});
+			}
+			console.log(predictedGrades);
 		}
+		else delete predictedGrades[subject.name];
+		updateSlots();
+	});
+}
+
+var longestDuration = 0;
+function updateSlots(full = false) {
+	$('#slots').empty();
+	if (Object.keys(predictedGrades).length > 0) {
+		canSpin = true;
+		$('#slots').css('display', 'flex');
+		$('#spin-button').text('Get My Grades!');
+		$('#spin-button').css('cursor', 'pointer');
+	}
+	else {
+		canSpin = false;
+		$('#slots').css('display', 'none');
+		$('#spin-button').text('v Select Your Predicted Grades v');
+		$('#spin-button').css('cursor', 'not-allowed');
+	}
+
+	for (let [subjectName, predictedGrade] of Object.entries(predictedGrades)) {
+		let slot = $('<div class="slot"></div>');
+		slot.append(`<div class="slot-title">${subjectName}</div>`);
+
+		let slotGradesWrapper = $(`<div class="slot-grades-wrapper"></div>`);
+
+		if (full) {
+			let duration = Math.random()*4+4;
+			if (duration > longestDuration) longestDuration = duration;
+			var slotGrades = $(`<div class="slot-grades" style="animation-duration: ${duration}s;"></div>`);
+		}
+		else {
+			var slotGrades = $(`<div class="slot-grades"></div>`);
+		}
+		
+		if (full) {
+			for (let z = 0; z < 41; z++) {
+				let grade = '';
+				let randomNumber = Math.floor(Math.random()*predictedGrade.total);
+				for (_grade of predictedGrade.grades) {
+					if (randomNumber <= _grade.total) {
+						grade = _grade.grade;
+						break;
+					}
+				}
 	
-		slot.append(slotGrades);
+				if (z === 39) slotGrades.append(`<div class="slot-grade slot-grade-actual">${grade}</div>`);
+				else slotGrades.append(`<div class="slot-grade slot-grade-fill">${grade}</div>`);
+			}
+		}
+		else {
+			for (let z = 0; z < 3; z++) {
+				if (z === 1) slotGrades.append(`<div class="slot-grade">${predictedGrade.grade}</div>`);
+				else slotGrades.append(`<div class="slot-grade slot-grade-fill">?</div>`);
+			}
+		}
+		
+	
+		slotGradesWrapper.append(slotGrades);
+		slot.append(slotGradesWrapper);
 		$('#slots').append(slot)
 	}
 	$('#slots').append('<div class="slot-target"></div>');
+}
+
+function spin() {
+	if (!canSpin) return;
+	canSpin = false;
+
+	updateSlots(true);
+
+	$('.slot-grades').css('animation-name', 'spin');
 
 	setTimeout(() => {
 		$('.slot-grades').css('duration', 0);
@@ -77,7 +176,7 @@ function spin() {
 		setTimeout(() => {
 			$('#spin-button').text('Respin!');
 			$('#spin-button').css('cursor', 'pointer');
-			spinning = false;
+			canSpin = true;
 		}, 2000);
 	}, longestDuration*1000);
 }
